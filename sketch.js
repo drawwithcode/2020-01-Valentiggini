dba41392dd9c0c825192244b6e95fbb1ea59996d
valentiggini <pippia.valentina@gmail.com>
2020-09-30 00:39:23 +0200

    Explosive Rose

--- sketch.js
+++ sketch.js
@@ -1,12 +1,42 @@
 function preload(){
-  // put preload code here
+
 }
+//The explosive Rose
+
+var k=5/8;
+
 
 function setup() {
   createCanvas(windowWidth,windowHeight)
-  // put setup code here
+
 }
 
+beginShape();
 function draw() {
-  // put drawing code here
+
+
+    background('#24003D')
+        translate(width/2, height/2);
+
+
+
+    noFill();
+    stroke(lerpColor(color('blue'), color('red'), frameCount/60));
+    rotate(frameCount * 6);
+    strokeWeight(1);
+
+
+
+if (frameCount < 60) {
+  for (var a = 0; a < TWO_PI*8; a += 0.02) {
+    var r = frameCount*60*cos(k*a);
+    var x = r*cos(a);
+    var y = r*sin(a);
+
+    vertex(x,y);
+  }
+
+
+  }
+  endShape(CLOSE);
 }

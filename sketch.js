3368796a99c9987578de9b736328d8580d2dde48
Valentiggini <71349260+Valentiggini@users.noreply.github.com>
2020-10-14 02:07:12 +0200

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,19 +16,19 @@ function draw() {
 
 
     background('#24003D')
-        translate(width/2, height/2);
+    translate(width/2, height/2);
 
 
 
     noFill();
     stroke(lerpColor(color('blue'), color('red'), frameCount/60));
-    rotate(frameCount * 6);
+    //rotate(frameCount * 6);
     strokeWeight(1);
 
 
 
 if (frameCount < 60) {
-  for (var a = 0; a < TWO_PI*8; a += 0.02) {
+  for (var a = 0; a < TWO_PI*8.7; a += 0.02) {
     var r = frameCount*60*cos(k*a);
     var x = r*cos(a);
     var y = r*sin(a);
